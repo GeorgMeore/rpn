02f367f9d7ca5f719c1e8c0e0ea075ea6513728b
georgii <>
2023-04-26 18:50:42 +0300

    implemented unary operators

--- main.go
+++ main.go
@@ -1,6 +1,5 @@
 // Don't know why I did this but I did
 
-// TODO: add support for unary opearators (or not)
 // TODO: write README
 
 package main
@@ -27,43 +26,57 @@ type operator struct {
 	sym   string // operator string
 	prec  int    // precedence (>= 0)
 	right bool   // associativity (false - left, true - right)
+	unary bool   // arity (false - binary, true - unary)
 }
 
-// NOTE: operators with the same precedence must have the same associativity
-func (o1 operator) less(o2 operator) bool {
-	return o2.prec > o1.prec || o1.prec == o2.prec && !o2.right
-}
-
+// TODO: some research (I am not sure this is a correct extension to shunting yard, but it seems to work)
 // convert infix expression to postfix
 func toRPN(expr []string, opmap map[string]operator) []string {
-	rpn, opstack := []string{}, []operator{}
+	rpn, ops := []string{}, []operator{}
 	for _, s := range expr {
-		if op, isop := opmap[s]; isop {
-			i := len(opstack) - 1
-			for ; i >= 0 && op.less(opstack[i]); i-- {
-				rpn = append(rpn, opstack[i].sym)
-			}
-			opstack = append(opstack[:i+1], op)
-		} else if s == "(" {
+		if s == "(" {
 			// push pseudo-operator '(' with lowest precedence
-			opstack = append(opstack, operator{sym: "(", prec: -1})
+			ops = append(ops, operator{sym: "(", prec: -1})
 		} else if s == ")" {
-			i := len(opstack) - 1
-			for ; i > 0 && opstack[i].sym != "("; i-- {
-				rpn = append(rpn, opstack[i].sym)
+			i := len(ops) - 1
+			for ; i > 0 && ops[i].sym != "("; i-- {
+				rpn = append(rpn, ops[i].sym)
 			}
-			opstack = opstack[:i]
-		} else {
+			ops = ops[:i]
+		} else if op, isop := opmap[s]; !isop {
 			rpn = append(rpn, s)
+		} else if op.unary {
+			if op.right {
+				ops = append(ops, op)
+			} else {
+				i := len(ops) - 1
+				for ; i >= 0 && (ops[i].unary && ops[i].prec<op.prec || !ops[i].unary && ops[i].prec>op.prec); i-- {
+					rpn = append(rpn, ops[i].sym)
+				}
+				ops = ops[:i+1]
+				rpn = append(rpn, op.sym)
+			}
+		} else {
+			i := len(ops) - 1
+			if op.right {
+				for ; i >= 0 && ops[i].prec > op.prec; i-- {
+					rpn = append(rpn, ops[i].sym)
+				}
+			} else {
+				for ; i >= 0 && ops[i].prec >= op.prec; i-- {
+					rpn = append(rpn, ops[i].sym)
+				}
+			}
+			ops = append(ops[:i+1], op)
 		}
 	}
-	for i := len(opstack) - 1; i >= 0; i-- {
-		rpn = append(rpn, opstack[i].sym)
+	for i := len(ops) - 1; i >= 0; i-- {
+		rpn = append(rpn, ops[i].sym)
 	}
 	return rpn
 }
 
-// split line by spaces and brackets
+// split line by spaces and parentheses
 func split(s string) []string {
 	tokens := []string{}
 	for pos, start := 0, 0; pos <= len(s); pos++ {
@@ -83,7 +96,7 @@ func split(s string) []string {
 	return tokens
 }
 
-// join words with space
+// join strings with space
 func join(sep string, ss []string) string {
 	if len(ss) == 0 {
 		return ""
@@ -93,6 +106,41 @@ func join(sep string, ss []string) string {
 		joined = joined + sep + s
 	}
 	return joined
+}
+
+// "expected" flags
+const (
+	fnothing = 1 << iota
+	frparen
+	flparen
+	farg
+	finfix
+	fprefix
+	fpostfix
+)
+
+// check if i-th element of infix expression is something we expect
+func expect(flags int, opmap map[string]operator, infix []string, i int) bool {
+	if i < 0 || i == len(infix) {
+		return flags & fnothing != 0
+	}
+	if infix[i] == "(" {
+		return flags & flparen != 0
+	}
+	if infix[i] == ")" {
+		return flags & frparen != 0
+	}
+	op, isop := opmap[infix[i]]
+	if !isop {
+		return flags & farg != 0
+	}
+	if !op.unary {
+		return flags & finfix != 0
+	}
+	if !op.right {
+		return flags & fpostfix != 0
+	}
+	return flags & fprefix != 0
 }
 
 // TODO: error location reporting
@@ -113,19 +161,35 @@ func check(infix []string, opmap map[string]operator) error {
 				return errorf("unmatched ')'")
 			}
 			parens -= 1
-		} else if has(opmap, s) {
-			if !(i > 0 && (!has(opmap, infix[i-1]) || infix[i-1] == ")")) {
-				return errorf("expected a ')' or an argument before '%s'", s)
+		} else if op, isop := opmap[s]; !isop {
+			if !expect(fnothing | flparen | finfix | fprefix, opmap, infix, i - 1) {
+				return errorf("expected nothing or a '(' or an binary or prefix operator before '%s'", s)
 			}
-			if !(i < len(infix)-1 && (!has(opmap, infix[i+1]) || infix[i+1] == "(")) {
-				return errorf("expected a '(' or an argument after '%s'", s)
+			if !expect(fnothing | frparen | finfix | fpostfix, opmap, infix, i + 1) {
+				return errorf("expected nothing or a ')' or an binary or postfix operator after '%s'", s)
+			}
+		} else if op.unary {
+			if op.right {
+				if !expect(fnothing | flparen | finfix | fprefix, opmap, infix, i - 1) {
+					return errorf("expected nothing or a '(' or an binary or prefix operator before '%s'", s)
+				}
+				if !expect(flparen | farg | fprefix, opmap, infix, i + 1) {
+					return errorf("expected a '(' or an argument or a prefix operator after '%s'", s)
+				}
+			} else {
+				if !expect(frparen | farg | fpostfix, opmap, infix, i - 1) {
+					return errorf("expected a ')' or an argument or a postfix operator before '%s'", s)
+				}
+				if !expect(fnothing | frparen | finfix | fpostfix, opmap, infix, i + 1) {
+					return errorf("expected nothing or a ')' or an binary or postfix operator after '%s'", s)
+				}
 			}
 		} else {
-			if !(i == 0 || (has(opmap, infix[i-1]) || infix[i-1] == "(")) {
-				return errorf("expected nothing or a '(' or an operator before '%s'", s)
+			if !expect(frparen | farg | fpostfix, opmap, infix, i - 1) {
+				return errorf("expected a ')' or an argument or a postfix operator before '%s'", s)
 			}
-			if !(i == len(infix)-1 || (has(opmap, infix[i+1]) || infix[i+1] == ")")) {
-				return errorf("expected nothing or a ')' or an operator after '%s'", s)
+			if !expect(flparen | farg | fprefix, opmap, infix, i + 1) {
+				return errorf("expected a '(' or an argument or a prefix operator after '%s'", s)
 			}
 		}
 	}
@@ -143,12 +207,16 @@ func getops(args []string) ([]operator, error) {
 		if len(desc) < 2 {
 			return nil, errorf("'%s': invalid format", arg)
 		}
-		right := false
+		right, unary := false, false
 		for _, flag := range desc[0] {
 			if flag == 'r' {
 				right = true
 			} else if flag == 'l' {
 				right = false
+			} else if flag == 'u' {
+				unary = true
+			} else if flag == 'b' {
+				unary = false
 			} else {
 				return nil, errorf("'%s': unknown flag: %c", arg, flag)
 			}
@@ -157,7 +225,7 @@ func getops(args []string) ([]operator, error) {
 			if len(sym) == 0 {
 				return nil, errorf("'%s': empty operator", arg)
 			}
-			ops = append(ops, operator{sym: sym, right: right, prec: prec})
+			ops = append(ops, operator{sym: sym, right: right, prec: prec, unary: unary})
 		}
 	}
 	return ops, nil
